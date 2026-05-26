a762515296cc1f774670bbf054dfe22b9eb72559
gopinath5262 <gopichowdary5262@gmail.com>
2026-05-26 12:15:01 +0530

    maven files added

---
+++ funnyapplication/funnyapplication/src/main/java/org/jsp/funnyapplication/FunnyapplicationApplication.java
@@ -0,0 +1,13 @@
+package org.jsp.funnyapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunnyapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunnyapplicationApplication.class, args);
+	}
+
+}
---
+++ funnyapplication/funnyapplication/src/test/java/org/jsp/funnyapplication/FunnyapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jsp.funnyapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunnyapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
