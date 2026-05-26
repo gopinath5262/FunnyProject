6a5a3ab87c725b6dd28e5899d69749e8a675aec0
gopinath5262 <gopichowdary5262@gmail.com>
2026-05-26 14:14:17 +0530

    images added and applied to background

--- funnyproject/src/Component/MainWebpage.jsx
+++ funnyproject/src/Component/MainWebpage.jsx
@@ -3,15 +3,23 @@ import { Link } from 'react-router-dom'
 const MainWebpage = () => {
   return (
     <div>
-        <h1>Welcome to Our Webpage Dear !😉😉</h1>
-        <h1>Thank you ! to continue please do select below ⬇👇👇🏻😁</h1>
-        <Link to="/login">
-        <button>Login</button>
-        </Link>
-        <Link to="/signup">
-         <button type="button" class="btn btn-primary">SIGN-IN</button></Link>
+        <h1 style={{color:"black"}}>&nbsp;Welcome!😉😉</h1>
+        <h1 style={{color:"green"}}>&nbsp;Thank you !😁 to continue please  select  ⬆🙋🏻‍♂️</h1>
+        
          
-    </div>
+  
+   <Link to="/login">
+        
+        <button class="btn btn-success "> &nbsp;LOGIN 🤩</button>
+        </Link>
+        <br />
+        <br />
+        <Link to="/signup">
+         <button type="button" class="btn btn-primary">&nbsp; SIGN-IN 🫱🏻‍🫲🏻</button>
+        </Link>
+         
+  </div>
+
   )
 }
 
