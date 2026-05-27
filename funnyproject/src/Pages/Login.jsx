0c60fbe44c1deadd685863cde25a3b0e14f612aa
gopinath5262 <gopichowdary5262@gmail.com>
2026-05-27 10:39:54 +0530

    login page work done along with css

--- funnyproject/src/Pages/Login.jsx
+++ funnyproject/src/Pages/Login.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-      Login
+    <div className='login_form'>
+      <form action="POST">
+        <ul className='list_login'>
+        <h1 className='login_name'>LOGIN</h1>
+        <br />
+          Email please 😘
+        <input type="email" placeholder='enter email' />
+        Password🥲
+        <input type="password" placeholder='password also required'/>
+        <br />
+        <button class="btn btn-primary btn-lg ">Submit 🤗</button>
+        </ul>
+      </form>
     </div>
   )
 }
