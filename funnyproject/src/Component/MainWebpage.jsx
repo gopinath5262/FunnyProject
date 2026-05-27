c6256695d06a363dff31fdbfd6d51121a3df1287
gopinath5262 <gopichowdary5262@gmail.com>
2026-05-27 10:14:36 +0530

    mainWebpage completed

--- funnyproject/src/Component/MainWebpage.jsx
+++ funnyproject/src/Component/MainWebpage.jsx
@@ -2,23 +2,21 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 const MainWebpage = () => {
   return (
-    <div>
-        <h1 style={{color:"black"}}>&nbsp;Welcome!😉😉</h1>
-        <h1 style={{color:"green"}}>&nbsp;Thank you !😁 to continue please  select  ⬆🙋🏻‍♂️</h1>
-        
-         
-  
-   <Link to="/login">
-        
-        <button class="btn btn-success "> &nbsp;LOGIN 🤩</button>
-        </Link>
-        <br />
-        <br />
-        <Link to="/signup">
-         <button type="button" class="btn btn-primary">&nbsp; SIGN-IN 🫱🏻‍🫲🏻</button>
-        </Link>
+    <div className='main_page_form'>
+      <div >
+   <ul className='list'>
+    <h1 style={{color:"black"}}>Welcome!😉😉</h1>
+   <li ><Link to="/login">
+        <button class="btn btn-primary btn-lg login" > &nbsp;LOGIN 🤩</button>
+    </Link>🦖</li>
+       <br/>
+    <li><Link to="/signup">
+         <button type="button" class="btn btn-dark btn-primary btn-lg signup">&nbsp; SIGN-IN 🫱🏻‍🫲🏻</button>
+    </Link>🐉</li>
+   </ul>
          
   </div>
+    </div>
 
   )
 }
